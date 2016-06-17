79fd2d8b50ec6597db1b50c0950ef6a84ba38845
root <root@solydxk>
2016-06-17 09:50:32 +0200

    Changed search engine to Yahoo (was Bing)

--- etc/skel/.thunderbird/pjzwmea6.default/prefs.js
+++ etc/skel/.thunderbird/pjzwmea6.default/prefs.js
@@ -2,6 +2,7 @@ user_pref("mail.biff.show_tray_icon", false);
 user_pref("app.update.enabled", false);
 user_pref("extensions.autoDisableScopes", 0);
 user_pref("extensions.enabledScopes", 15);
+user_pref("browser.search.order.1", "Yahoo");
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"PanelUI-contents\":[\"edit-controls\",\"zoom-controls\",\"new-window-button\",\"privatebrowsing-button\",\"save-page-button\",\"print-button\",\"history-panelmenu\",\"fullscreen-button\",\"find-button\",\"preferences-button\",\"add-ons-button\",\"developer-button\",\"simplels-widget\"]}}");
 user_pref("extensions.firetray.app_browser_icon_names", "[\"web-browser\",\"firefox\"]");
 user_pref("extensions.firetray.firstrun", false);
--- templates/prefs.js
+++ templates/prefs.js
@@ -2,3 +2,4 @@ user_pref("mail.biff.show_tray_icon", false);
 user_pref("app.update.enabled", false);
 user_pref("extensions.autoDisableScopes", 0);
 user_pref("extensions.enabledScopes", 15);
+user_pref("browser.search.order.1", "Yahoo");
